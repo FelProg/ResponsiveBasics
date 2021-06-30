b965a3fb937099643a896bb1d03135fdd5424ce4
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2021-06-30 11:26:17 -0700

    menu collapser when resizing

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
 
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--visible");
+});
 
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--visible');
-})
+window.onresize = () => {
+  if (window.innerWidth >= 800) nav.classList.remove("nav--visible");
+};
+
+// window.onresize = CollapseMenu;
